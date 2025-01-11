b6a4bdf8c799675331cc2ee01e46c29a8af446a6
Mahaut Latinis <mahautlatinisheritier@gmail.com>
2025-01-11 21:01:09 +0100

    Testing actions (#11)

--- backend/drizzle/db.ts
+++ backend/drizzle/db.ts
@@ -1,6 +1,5 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import * as schema from "./schema"
-import postgres from "postgres"
 
 const client = Deno.env.get("DATABASE_URL") || "";
 export const db = drizzle(client,{ schema, logger: true})
